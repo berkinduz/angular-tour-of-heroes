0d138a8bf3a85bfb10e95d7a36a92ea58c92fe85
berkinduz <duzberkin@gmail.com>
2022-02-03 19:56:53 +0300

    hero detail template transferred to own component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,24 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes'
 
+import {HeroService} from '../hero.service'
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero:Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
