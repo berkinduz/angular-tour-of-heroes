e3b8e81fa24662948a25ef6b2e0299674561cca1
Berkin Düz <64802864+berkinduz@users.noreply.github.com>
2022-02-04 12:11:04 +0300

    dashrobard component route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HeroesComponent } from './heroes/heroes.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
-];
+  { path: 'dashboard', component: DashboardComponent },
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
